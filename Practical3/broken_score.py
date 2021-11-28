eb773b01496929bd85865137da4ea710283f55a8
Dieu Linh <dieulinh.tran@my.jcu.edu.au>
2021-11-28 16:02:25 +0800

    The updated program, about printing student scores

--- Practical3/broken_score.py
+++ Practical3/broken_score.py
@@ -1,14 +1,17 @@
-"""
-CP1404/CP5632 - Practical
-Broken program to determine score status
-"""
+def main():
+    score = float(input("Enter score: "))
+    Grade(score)
 
-score = float(input("Enter score: "))
-if score > 100 or score < 0:
-    print("Invalid score")
-elif score >= 50:
-    print("Passable")
-elif score >= 90:
-    print("Excellent")
-else:
-    print("Bad")
+
+def Grade(score):
+    if score > 100 or score < 0:
+        print("Invalid score")
+    elif score >= 50:
+        print("Passable")
+    elif score >= 90:
+        print("Excellent")
+    else:
+        print("Bad")
+
+
+main()
