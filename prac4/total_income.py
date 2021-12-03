f13794d598aa04de01c6dcaca770a2f01d608180
Dieu Linh <dieulinh.tran@my.jcu.edu.au>
2021-12-03 16:15:09 +0800

    rename the input of number of months, reformat the income input to the format string, insert an income_report function.

--- prac4/total_income.py
+++ prac4/total_income.py
@@ -7,18 +7,22 @@ Starter code for cumulative total income program
 def main():
     """Display income report for incomes over a given number of months."""
     incomes = []
-    months = int(input("How many months? "))
+    number_of_months = int(input("How many months? "))
 
-    for month in range(1, months + 1):
-        income = float(input("Enter income for month " + str(month) + ": "))
+    for month in range(1, number_of_months + 1):
+        income = float(input("Enter income for month {:.0f}: ".format(month)))
         incomes.append(income)
 
+    income_report(incomes, number_of_months)
+
+
+def income_report(incomes, number_of_months):
     print("\nIncome Report\n-------------")
     total = 0
-    for month in range(1, months + 1):
+    for month in range(1, number_of_months + 1):
         income = incomes[month - 1]
         total += income
-        print("Month {:2} - Income: ${:10.2f} Total: ${:10.2f}".format(month, income, total))
+        print("Month {:1} - Income: ${:5.2f}     Total: ${:5.2f}".format(month, income, total))
 
 
 main()
