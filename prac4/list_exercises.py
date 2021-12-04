4749d98726805882fb3592ed12bcc0c4748a795f
Dieu Linh <dieulinh.tran@my.jcu.edu.au>
2021-12-04 15:35:24 +0800

    ask the user for their input then print out several values from the list then second part check if the username is on the list

---
+++ prac4/list_exercises.py
@@ -0,0 +1,20 @@
+# 1
+numbers = []
+for i in range(5):
+    number = int(input(" Number: "))
+    numbers.append(number)
+print(f" the first number is: {numbers[0]}")
+print(f" the last number is: {numbers[4]}")
+print(f" the smallest number is {min(numbers)}")
+print(f" the largest number is {max(numbers)}")
+average = sum(numbers) / len(numbers)
+print(f" the average number is {average}")
+
+# 2
+usernames = ['jimbo', 'giltson98', 'derekf', 'WhatSup', 'NicolEye', 'swei45', 'BaseInterpreterInterface', 'BaseStdIn',
+             'Command', 'ExecState', 'InteractiveConsole', 'InterpreterInterface', 'StartServer', 'bob']
+username = input(" What's your name? ")
+if username in usernames:
+    print(" Access granted")
+else:
+    print(" Access denied")
