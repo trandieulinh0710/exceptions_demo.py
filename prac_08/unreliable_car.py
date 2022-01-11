43a26287eeed8b86deeebdf9ce1cb933af45b4aa
Dieu Linh <dieulinh.tran@my.jcu.edu.au>
2022-01-11 17:57:04 +0800

    add a random number to the drive function and new atribute to init of unreliable car then test with unreliable test

---
+++ prac_08/unreliable_car.py
@@ -0,0 +1,16 @@
+from prac_08.car import Car
+import random
+
+
+class UnreliableCar(Car):
+    
+    def __init__(self, name, fuel, reliability):
+        super().__init__(name, fuel)
+        self.reliability = reliability
+
+    def drive(self, distance):
+        random_number = random.randint(1, 100)
+        if random_number >= self.reliability:
+            distance = 0
+        distance_driven = super().drive(distance)
+        return distance_driven
